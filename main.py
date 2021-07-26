71b5725d613d877cc37eb534f6e0beb38bef9a5c
louistec90 <lewsdev@gmail.com>
2021-07-25 20:10:23 -0500

    Agregada la lectura desde un csv

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 # ESCRIBIR POR WASAP BOT
 
 import pywhatkit as pwk
+import csv
+from datetime import datetime
 
-#pwk.sendwhats_image(phone_no="+1123654987", img_path="/home/luiscs/Downloads/giphy.gif", caption="Mensaje de prueba de gif")
+def get_numbers(filename):
+    result = []
+    with open(filename, 'r') as csv_file:
+        numbers = csv.reader(csv_file)
+        for row in numbers:
+            # print(row[0])
+            result.append(row[0])
+    return result
 
 
-pwk.sendwhatmsg(phone_no='+51987654321', message='Hola este es un mensaje de prueba', time_hour=23, time_min=29 ,tab_close=True)
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("current time: " + current_time)
+h, m, s = current_time.split(':')
+print("Hora: {} - Minuto: {} - Segundo: {}".format(h,m,s))
+
+# pwk.sendwhats_image(phone_no="+1123654987", img_path="/home/luiscs/Downloads/giphy.gif", caption="Mensaje de prueba de gif")
+
+phone_number = get_numbers("mynumber.csv")
+
+try:
+    pwk.sendwhatmsg(phone_number[0], 'Hola este es un mensaje de prueba', int(h), int(m) + 1 ,tab_close=True)
+    print("Envio exitoso")
+except:
+    print("Ha ocurrido un error")    
 
 
