8c3079373df504dda37b37070ec7949264ead16e
louistec90 <lewsdev@gmail.com>
2021-07-31 11:34:41 -0500

    formateo

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pywhatkit as pwk
 import csv
 from datetime import datetime
 
+
+
 def get_numbers(filename):
     result = []
     with open(filename, 'r') as csv_file:
@@ -19,16 +21,15 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("current time: " + current_time)
 h, m, s = current_time.split(':')
-print("Hora: {} - Minuto: {} - Segundo: {}".format(h,m,s))
+print("Hora: {} - Minuto: {} - Segundo: {}".format(h, m, s))
 
-# pwk.sendwhats_image(phone_no="+1123654987", img_path="/home/luiscs/Downloads/giphy.gif", caption="Mensaje de prueba de gif")
+# pwk.sendwhats_image(phone_no="+1123654987", img_path="/home/luiscs/Downloads/giphy.gif", caption="Mensaje de prueba
+# de gif")
 
 phone_number = get_numbers("mynumber.csv")
 
 try:
-    pwk.sendwhatmsg(phone_number[0], 'Hola este es un mensaje de prueba', int(h), int(m) + 1 ,tab_close=True)
+    pwk.sendwhatmsg(phone_number[0], 'Hola este es un mensaje de prueba', int(h), int(m) + 1, tab_close=True)
     print("Envio exitoso")
 except:
-    print("Ha ocurrido un error")    
-
-
+    print("Ha ocurrido un error")
